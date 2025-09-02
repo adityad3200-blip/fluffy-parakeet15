0e42a5afc4365c871b5e666a6e3ab0648f5f055d
adityad3200-blip <adityad3200@gmail.com>
2025-09-02 16:57:11 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+function handleSubmit(e){e.preventDefault();alert('Thanks! Your message has been noted. Please email adityad48@gmail.com for the fastest response.');return false;}
+document.getElementById('year').textContent = new Date().getFullYear();
